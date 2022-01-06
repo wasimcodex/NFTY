97afed97dac728ea38d1b561fc6fe5ae73e367d7
wasim <wasimcodex@gmail.com>
2022-01-07 00:34:29 +0530

    NFT card update

--- frontend/src/components/NftCards.jsx
+++ frontend/src/components/NftCards.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Row, Col, Card } from 'react-bootstrap'
+import eth from '../assets/eth.svg'
+import { Row, Col, Card, Container } from 'react-bootstrap'
 
 const NftCards = ({ NFTArray }) => {
   return (
@@ -8,10 +9,33 @@ const NftCards = ({ NFTArray }) => {
         {NFTArray.map((nft, index) => (
           <Col>
             <Card>
-              <Card.Img variant="top" src={nft.image} />
+              <Card.Img className="cardImg" variant="top" src={nft.image} />
               <Card.Body>
-                <Card.Title>{nft.name + ' #' + (index + 1)}</Card.Title>
-                <Card.Text>{nft.description}</Card.Text>
+                <Container style={{ padding: '0px' }}>
+                  <Row>
+                    <Col xs={8}>
+                      <Card.Title style={{ fontSize: '16px' }}>
+                        {nft.name}{' '}
+                        <span style={{ color: 'magenta' }}>
+                          {'#' + (index + 1)}
+                        </span>
+                      </Card.Title>
+                      <Card.Text style={{ fontSize: '16px', color: 'grey' }}>
+                        {nft.description}
+                      </Card.Text>
+                    </Col>
+                    <Col style={{ textAlign: 'right', fontSize: '16px' }}>
+                      <p>
+                        Price <br />
+                        <img
+                          style={{ height: '20px', marginRight: '5px' }}
+                          src={eth}
+                        ></img>
+                        0.01
+                      </p>
+                    </Col>
+                  </Row>
+                </Container>
               </Card.Body>
             </Card>
           </Col>
