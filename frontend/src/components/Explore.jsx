d5cb1d206d362ba3cc164cef1a0e89c8fc8c550b
wasim <wasimcodex@gmail.com>
2022-01-22 03:24:29 +0530

    Getting explore page data from opensea API

--- frontend/src/components/Explore.jsx
+++ frontend/src/components/Explore.jsx
@@ -3,92 +3,34 @@
  */
 
 import React, { useEffect, useState } from 'react'
+import { Row, Col } from 'react-bootstrap'
 import NftCards from './NftCards'
 
-const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY
-const { createAlchemyWeb3 } = require('@alch/alchemy-web3')
-const web3 = createAlchemyWeb3(alchemyKey)
+const axios = require('axios')
 
-const { abi } = require('../contracts/NFTY.json')
 const { nftAddress } = require('../contracts/nft-contract-address.json')
 
 //Gets NFT collections details (id, name, description, owner)
 const getNFTs = async () => {
-  window.contract = await new web3.eth.Contract(abi, nftAddress)
-  const transactionParameters = {
-    to: nftAddress,
-    from: window.ethereum.selectedAddress,
-    data: window.contract.methods.getCount().encodeABI(),
-  }
-
-  try {
-    const res = await window.ethereum.request({
-      method: 'eth_call',
-      params: [transactionParameters, 'latest'],
+  const data = await axios
+    .get(
+      `https://testnets-api.opensea.io/api/v1/assets?asset_contract_addresses=${nftAddress}&order_direction=desc&offset=0&limit=20`,
+    )
+    .then((res) => {
+      return res.data.assets
     })
-
-    const count = parseInt(res, 16)
-    console.log('count : ' + count)
-
-    const urls = []
-    const owners = []
-    for (let i = 1; i <= count; i++) {
-      const txParameters = {
-        to: nftAddress,
-        from: window.ethereum.selectedAddress,
-        data: window.contract.methods.tokenURI(i).encodeABI(),
-      }
-
-      const OwnParameters = {
-        to: nftAddress,
-        from: window.ethereum.selectedAddress,
-        data: window.contract.methods.ownerOf(i).encodeABI(),
-      }
-
-      const response = await window.ethereum.request({
-        method: 'eth_call',
-        params: [txParameters, 'latest'],
-      })
-      let url = web3.utils.hexToAscii(response)
-      urls.push(url.trim().substring(64, 144))
-
-      const OwnResponse = await window.ethereum.request({
-        method: 'eth_call',
-        params: [OwnParameters, 'latest'],
-      })
-
-      let OwnerAdd = web3.utils.hexToAscii(OwnResponse)
-      owners.push(OwnerAdd)
-    }
-    console.log(urls)
-    console.log(owners)
-
-    const data = []
-    for (let i = 0; i < urls.length; i++) {
-      const res = await fetch(urls[i])
-        .then((response) => response.json())
-        .then((res) => {
-          data.push(res)
-        })
-    }
-    console.log(data)
-
-    return data
-  } catch (err) {
-    return {
-      success: false,
-      status: 'somethig went wrong ' + err.message,
-    }
-  }
+    .catch((err) => {
+      console.log(err)
+    })
+  return data
 }
 
 export default function Explore() {
   const [NFTs, setNFTs] = useState([])
   useEffect(() => {
     async function getNfts() {
-      const NFTArray = await getNFTs()
-      setNFTs(NFTArray)
-      console.log(NFTs)
+      const nfts = await getNFTs()
+      setNFTs(nfts)
     }
     getNfts()
   }, [])
@@ -96,7 +38,15 @@ export default function Explore() {
   const [Status, setStatus] = useState('')
   return (
     <div>
-      <NftCards NFTArray={NFTs} />
+      <div style={{ padding: '20px' }}>
+        <Row xs={1} md={3} className="g-4">
+          {NFTs.map((nft, index) => (
+            <Col key={index}>
+              <NftCards nft={nft} key={index} />
+            </Col>
+          ))}
+        </Row>
+      </div>
     </div>
   )
 }
--- frontend/src/components/NftCards.jsx
+++ frontend/src/components/NftCards.jsx
@@ -2,46 +2,37 @@ import React from 'react'
 import eth from '../assets/eth.svg'
 import { Row, Col, Card, Container } from 'react-bootstrap'
 
-const NftCards = ({ NFTArray }) => {
+const NftCards = ({ nft }) => {
+  const index = 1
   return (
-    <div style={{ padding: '20px' }}>
-      <Row xs={1} md={3} className="g-4">
-        {NFTArray.map((nft, index) => (
-          <Col>
-            <Card>
-              <Card.Img className="cardImg" variant="top" src={nft.image} />
-              <Card.Body>
-                <Container style={{ padding: '0px' }}>
-                  <Row>
-                    <Col xs={8}>
-                      <Card.Title style={{ fontSize: '16px' }}>
-                        {nft.name}{' '}
-                        <span style={{ color: 'magenta' }}>
-                          {'#' + (index + 1)}
-                        </span>
-                      </Card.Title>
-                      <Card.Text style={{ fontSize: '16px', color: 'grey' }}>
-                        {nft.description}
-                      </Card.Text>
-                    </Col>
-                    <Col style={{ textAlign: 'right', fontSize: '16px' }}>
-                      <p>
-                        Price <br />
-                        <img
-                          style={{ height: '20px', marginRight: '5px' }}
-                          src={eth}
-                        ></img>
-                        0.01
-                      </p>
-                    </Col>
-                  </Row>
-                </Container>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </div>
+    <Card>
+      <Card.Img className="cardImg" variant="top" src={nft.image_url} />
+      <Card.Body>
+        <Container style={{ padding: '0px' }}>
+          <Row>
+            <Col xs={8}>
+              <Card.Title style={{ fontSize: '16px' }}>
+                {nft.name}{' '}
+                <span style={{ color: 'magenta' }}>{'#' + nft.token_id}</span>
+              </Card.Title>
+              <Card.Text style={{ fontSize: '16px', color: 'grey' }}>
+                {nft.description}
+              </Card.Text>
+            </Col>
+            <Col style={{ textAlign: 'right', fontSize: '16px' }}>
+              <p>
+                Price <br />
+                <img
+                  style={{ height: '20px', marginRight: '5px' }}
+                  src={eth}
+                ></img>
+                0.01
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </Card.Body>
+    </Card>
   )
 }
 
