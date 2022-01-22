12650e1b459d6fd37fa1c1952e117bed53f04825
Tejas Sawant <75907043+SawantTejas@users.noreply.github.com>
2022-01-22 23:18:25 +0530

    Deploy Scripts

---
+++ scripts/deploy-Auction.js
@@ -0,0 +1,41 @@
+const { ethers, artifacts } = require('hardhat');
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log('Deploying Auction contract with account:', deployer.address);
+
+  const Auction = await ethers.getContractFactory('Auction');
+  const auction = await Auction.deploy();
+
+  console.log('Contract Auction deployed at address: ', auction.address);
+
+  saveArtifacts(auction);
+}
+
+const saveArtifacts = (auction) => {
+  const fs = require('fs');
+  const contractDir = __dirname + '/../frontend/src/artifacts';
+
+  if (!fs.existsSync(contractDir)) {
+    fs.mkdirSync(contractDir);
+  }
+
+  const auctionArtifact = artifacts.readArtifactSync('Auction');
+
+  const artifact = {
+    address: auction.address,
+    abi: auctionArtifact.abi,
+  };
+
+  console.log('Saving artifacts to:', contractDir);
+
+  fs.writeFileSync(contractDir + '/auction.json', JSON.stringify(artifact));
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(0);
+  });
