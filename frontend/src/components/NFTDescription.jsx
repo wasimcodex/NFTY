afa7122feeed17f9c05395216f35810918b26e15
wasim <wasimcodex@gmail.com>
2022-01-23 03:32:31 +0530

    Responsiveness added to NFT description

--- frontend/src/components/NFTDescription.jsx
+++ frontend/src/components/NFTDescription.jsx
@@ -8,12 +8,13 @@ import Col from 'react-bootstrap/Col'
 import Table from 'react-bootstrap/Table'
 
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 import {
   FaAlignJustify,
   FaAlignLeft,
   FaAngleDown,
+  FaAngleUp,
   FaChartLine,
   FaExchangeAlt,
   FaFolder,
@@ -23,6 +24,8 @@ import {
 } from 'react-icons/fa'
 
 import noChartData from '../assets/no-chart-data.svg'
+import emptyAsks from '../assets/empty-asks.svg'
+import emptyBids from '../assets/empty-bids.svg'
 
 const axios = require('axios')
 
@@ -43,6 +46,10 @@ const getNftData = async (contractAddress, tokenId) => {
 function NFTDescription({ wallet }) {
   let { contractAddress, tokenId } = useParams()
   const [NFT, setNFT] = useState({})
+  const [showPriceHistory, setPriceHistor] = useState(true)
+  const [showListing, setListing] = useState(false)
+  const [showOffers, setOffers] = useState(false)
+  const [showDetails, setDetails] = useState(false)
   useEffect(() => {
     console.log(contractAddress, tokenId)
     async function getNft() {
@@ -78,89 +85,80 @@ function NFTDescription({ wallet }) {
                 className="itemDesc"
                 style={{ marginTop: '5%', alignItems: 'center' }}
               >
-                <Card>
-                  <Card.Body>
-                    <Card.Title>
-                      <FaAlignJustify />
-                      <span style={{ marginLeft: '5%' }}>Description</span>
-                    </Card.Title>
-                  </Card.Body>
-                  <hr style={{ margin: '0px' }} />
-                  <Card.Body>
-                    <Card.Text>{NFT.description}</Card.Text>
-                  </Card.Body>
-                  <hr style={{ margin: '0px' }} />
-                  <Card.Body>
-                    <Card.Title>
-                      <FaFolder />
-                      <span style={{ marginLeft: '5%' }}>
-                        About Untitled Collection
+                <div className="basePanel">
+                  <button className="basePanelHeader">
+                    <FaAlignJustify />
+                    <span style={{ marginLeft: '15px' }}>Description</span>
+                  </button>
+                  <div className="basePanelBody">
+                    <div className="panelContainer">
+                      <div className="panelContent">{NFT.description}</div>
+                    </div>
+                  </div>
+                  <div className="basePanelBody">
+                    <button
+                      className="basePanelHeader"
+                      style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                      }}
+                      onClick={() => setDetails(!showDetails)}
+                    >
+                      <span>
+                        <FaInfoCircle />
+                        <span style={{ marginLeft: '15px' }}>Details</span>
                       </span>
-                    </Card.Title>
-                  </Card.Body>
-                  <hr style={{ margin: '0px' }} />
-                  <Card.Body>
-                    <Card.Text>You own this collection</Card.Text>
-                  </Card.Body>
-                  <hr style={{ margin: '0px' }} />
-                  <Card.Body>
-                    <Card.Title>
-                      <FaInfoCircle />
-                      <span style={{ marginLeft: '5%' }}>Details</span>
-                      {/* <span style={{marginLeft: '70%'}}><FaAngleDown /></span> */}
-                    </Card.Title>
-                  </Card.Body>
-                  <hr style={{ margin: '0px' }} />
-                  <Card.Body>
-                    <Card.Text>
-                      <div
-                        style={{
-                          justifyContent: 'space-between',
-                          display: 'flex',
-                        }}
-                      >
-                        Contract Address
-                        <span style={{ textAlign: 'right', maxWidth: '365px' }}>
-                          {contractAddress.slice(0, 6)}...
-                          {contractAddress.slice(38)}
-                        </span>
+                      <FaAngleDown />
+                    </button>
+                  </div>
+                  {showDetails && (
+                    <div className="basePanelBody">
+                      <div className="panelContainer">
+                        <div className="panelContent">
+                          <div
+                            style={{
+                              display: 'flex',
+                              justifyContent: 'space-between',
+                            }}
+                          >
+                            <span>Contract Address</span>
+                            <span>
+                              {contractAddress.slice(0, 6)}...
+                              {contractAddress.slice(38)}
+                            </span>
+                          </div>
+                          <div
+                            style={{
+                              display: 'flex',
+                              justifyContent: 'space-between',
+                            }}
+                          >
+                            <span>Token ID</span>
+                            <span>{tokenId}</span>
+                          </div>
+                          <div
+                            style={{
+                              display: 'flex',
+                              justifyContent: 'space-between',
+                            }}
+                          >
+                            <span>Token Standard</span>
+                            <span>ERC721</span>
+                          </div>
+                          <div
+                            style={{
+                              display: 'flex',
+                              justifyContent: 'space-between',
+                            }}
+                          >
+                            <span>Blockchain</span>
+                            <span>Ethereum</span>
+                          </div>
+                        </div>
                       </div>
-                      <div
-                        style={{
-                          justifyContent: 'space-between',
-                          display: 'flex',
-                        }}
-                      >
-                        Token ID
-                        <span style={{ textAlign: 'right', maxWidth: '365px' }}>
-                          {tokenId}
-                        </span>
-                      </div>
-                      <div
-                        style={{
-                          justifyContent: 'space-between',
-                          display: 'flex',
-                        }}
-                      >
-                        Token Standard
-                        <span style={{ textAlign: 'right', maxWidth: '365px' }}>
-                          ERC721
-                        </span>
-                      </div>
-                      <div
-                        style={{
-                          justifyContent: 'space-between',
-                          display: 'flex',
-                        }}
-                      >
-                        Blockchain
-                        <span style={{ textAlign: 'right', maxWidth: '365px' }}>
-                          Ethereum
-                        </span>
-                      </div>
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </Col>
@@ -176,7 +174,11 @@ function NFTDescription({ wallet }) {
                   wallet == NFT.owner.address ? (
                     <div className="itemCollectionToolbarWrapper">
                       <div className="buttonGrp">
-                        <FaGift />
+                        <Link
+                          to={`/nft/transfer/${contractAddress}/${tokenId}`}
+                        >
+                          <FaGift style={{ fontSize: '18px' }} />
+                        </Link>
                       </div>
                     </div>
                   ) : (
@@ -207,62 +209,91 @@ function NFTDescription({ wallet }) {
             </section>
             <div className="itemFrame">
               <div className="basePanel">
-                <button className="basePanelHeader">
-                  <FaChartLine />
-                  <span style={{ marginLeft: '3%' }}>Price History</span>
+                <button
+                  className="basePanelHeader"
+                  style={{ display: 'flex', justifyContent: 'space-between' }}
+                  onClick={() => setPriceHistor(!showPriceHistory)}
+                >
+                  <span>
+                    <FaChartLine />
+                    <span style={{ marginLeft: '15px' }}>Price History</span>
+                  </span>
+                  {showPriceHistory ? <FaAngleUp /> : <FaAngleDown />}
                 </button>
-                <div className="basePanelBody">
-                  <div className="panelContainer">
-                    <div className="panelContent">
-                      <div className="priceHistoryContainer">
-                        <div className="priceHistoryGraph">
-                          <img src={noChartData} />
+                {showPriceHistory && (
+                  <div className="basePanelBody">
+                    <div className="panelContainer">
+                      <div className="panelContent">
+                        <div className="priceHistoryContainer">
+                          <div className="priceHistoryGraph">
+                            <img src={noChartData} />
+                          </div>
+                          <div className="noOrdersText">
+                            No item activity yet
+                          </div>
                         </div>
-                        <div className="noOrdersText">No item activity yet</div>
                       </div>
                     </div>
                   </div>
-                </div>
+                )}
               </div>
             </div>
             <div className="itemFrame">
               <div className="basePanel">
-                <button className="basePanelHeader">
-                  <FaTag />
-                  <span style={{ marginLeft: '3%' }}>Listings</span>
+                <button
+                  className="basePanelHeader"
+                  style={{ display: 'flex', justifyContent: 'space-between' }}
+                  onClick={() => setListing(!showListing)}
+                >
+                  <span>
+                    <FaTag />
+                    <span style={{ marginLeft: '15px' }}>Listings</span>
+                  </span>
+                  {showListing ? <FaAngleUp /> : <FaAngleDown />}
                 </button>
-                <div className="basePanelBody">
-                  <div className="panelContainer">
-                    <div className="panelContent">
-                      <div className="priceHistoryContainer">
-                        <div className="priceHistoryGraph">
-                          <img src={noChartData} />
+                {showListing && (
+                  <div className="basePanelBody">
+                    <div className="panelContainer">
+                      <div className="panelContent">
+                        <div className="priceHistoryContainer">
+                          <div className="priceHistoryGraph">
+                            <img src={emptyAsks} />
+                          </div>
+                          <div className="noOrdersText">No listings yet</div>
                         </div>
-                        <div className="noOrdersText">No listings yet</div>
                       </div>
                     </div>
                   </div>
-                </div>
+                )}
               </div>
             </div>
             <div className="itemFrame">
               <div className="basePanel">
-                <button className="basePanelHeader">
-                  <FaAlignLeft />
-                  <span style={{ marginLeft: '3%' }}>Offers</span>
+                <button
+                  className="basePanelHeader"
+                  style={{ display: 'flex', justifyContent: 'space-between' }}
+                  onClick={() => setOffers(!showOffers)}
+                >
+                  <span>
+                    <FaAlignLeft />
+                    <span style={{ marginLeft: '15px' }}>Offers</span>
+                  </span>
+                  {showOffers ? <FaAngleUp /> : <FaAngleDown />}
                 </button>
-                <div className="basePanelBody">
-                  <div className="panelContainer">
-                    <div className="panelContent">
-                      <div className="priceHistoryContainer">
-                        <div className="priceHistoryGraph">
-                          <img src={noChartData} />
+                {showOffers && (
+                  <div className="basePanelBody">
+                    <div className="panelContainer">
+                      <div className="panelContent">
+                        <div className="priceHistoryContainer">
+                          <div className="priceHistoryGraph">
+                            <img src={emptyBids} />
+                          </div>
+                          <div className="noOrdersText">No offers yet</div>
                         </div>
-                        <div className="noOrdersText">No offers yet</div>
                       </div>
                     </div>
                   </div>
-                </div>
+                )}
               </div>
             </div>
           </Col>
@@ -272,11 +303,11 @@ function NFTDescription({ wallet }) {
             <div className="basePanel">
               <button className="basePanelHeader">
                 <FaExchangeAlt />
-                <span style={{ marginLeft: '3%' }}>Item Activity</span>
+                <span style={{ marginLeft: '15px' }}>Item Activity</span>
               </button>
               <div className="basePanelBody">
                 <div className="panelContainer">
-                  <div className="panelContent" style={{ padding: '0' }}>
+                  <div className="panelContent">
                     <Table responsive borderless>
                       <thead>
                         <tr>
@@ -291,8 +322,8 @@ function NFTDescription({ wallet }) {
                         <tr>
                           <td>Minted</td>
                           <td>0.001 ETH</td>
-                          <td>Null addr</td>
-                          <td>you</td>
+                          <td>Null Address</td>
+                          <td>You</td>
                           <td>3 days ago</td>
                         </tr>
                       </tbody>
