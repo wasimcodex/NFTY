eb36826e35c8393baf5b1e341faa3b729827b75e
ZainabBubere <zainabbubere@gmail.com>
2022-02-14 21:41:30 +0530

    Make SellNFT Route dynamic Co-authored-by Tejas Sawant tsawant894@gmail.com

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ function App() {
         <Route path="/nft/transfer/:contractAddress/:tokenId/" exact>
           <Transfer />
         </Route>
-        <Route path="/sell">
+        <Route path="/nft/:contractAddress/:tokenId/sell">
           <SellNFT />
         </Route>
       </Router>
