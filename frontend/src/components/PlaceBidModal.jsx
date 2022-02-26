69e11a532779cd2fb19fdb0adfff464ebfb2894d
ZainabBubere <zainabbubere@gmail.com>
2022-02-26 10:56:30 +0530

    Create Place Bid Modal

---
+++ frontend/src/components/PlaceBidModal.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import {
+  Button,
+  Container,
+  FormControl,
+  InputGroup,
+  Col,
+  Form,
+  Card,
+  Row,
+  Alert,
+} from 'react-bootstrap'
+import ModalHeader from 'react-bootstrap/ModalHeader';
+import ModalTitle from 'react-bootstrap/ModalTitle';
+import ModalBody from 'react-bootstrap/ModalBody';
+
+function PlaceBidModal(props) {
+  return (
+    <Modal
+    {...props}
+    size="lg"
+    aria-labelledby="contained-modal-title-vcenter"
+    centered
+  >
+    <Modal.Header closeButton>
+      <Modal.Title id="contained-modal-title-vcenter">
+        Modal heading
+      </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <h4>Centered Modal</h4>
+      <p>
+        Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+        consectetur ac, vestibulum at eros.
+      </p>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button onClick={props.onHide}>Close</Button>
+    </Modal.Footer>
+  </Modal>
+  )
+}
+
+export default PlaceBidModal
