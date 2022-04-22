fd914be212e10b2f2c3250f8d54623cd7b129baf
Tejas Sawant <75907043+SawantTejas@users.noreply.github.com>
2022-04-22 14:27:30 +0530

    Transfer NFT integrated

--- frontend/src/components/Transfer.jsx
+++ frontend/src/components/Transfer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react';
 import {
   Row,
   Col,
@@ -8,10 +8,47 @@ import {
   InputGroup,
   Button,
 } from 'react-bootstrap'
-
+import { useParams } from 'react-router-dom'
+import { transferNft } from '../utils/nftFunctions'
 import imgplaceholder from '../assets/image-placeholder.png'
 
+
+const axios = require('axios');
+
+const getNftData = async (contractAddress, tokenId) => {
+  const data = await axios
+    .get(
+      `https://testnets-api.opensea.io/api/v1/asset/${contractAddress}/${tokenId}`,
+    )
+    .then((res) => {
+      return res.data
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  //console.log(data)
+  return data
+}
+
 const Transfer = () => {
+  let { contractAddress, tokenId } = useParams()
+  const [NFT, setNFT] = useState({})
+  const [recipient, setRecipient] = useState(null)
+
+  useEffect(() => {
+    async function getNft() {
+      const nft = await getNftData(contractAddress, tokenId)
+      // console.log(nft)
+      setNFT(nft)
+    }
+    getNft()
+  }, [])
+
+  const transfer = async () => {
+    const toAddress = recipient
+    const response = await transferNft(toAddress, tokenId)
+    alert(response.status)
+  }
   return (
     <div>
       <h2 style={{ textAlign: 'center' }}>Transfer</h2>
@@ -28,6 +65,7 @@ const Transfer = () => {
               <FormControl
                 placeholder="Enter Beneficiary Address Eg. 0x1234567..."
                 type="text"
+                onChange={e => setRecipient(e.target.value)}
               />
               <InputGroup.Text>Address</InputGroup.Text>
             </InputGroup>
@@ -37,6 +75,7 @@ const Transfer = () => {
           className="action-btn"
           variant="info"
           style={{ alignContent: 'center', margin: 'auto', display: 'block' }}
+          onClick={transfer}
         >
           Transfer
         </Button>
